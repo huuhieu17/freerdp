8185c9e8d5092273058e15ce8857c46d615fe7a8
Nguyen Huu Hieu <31349335+huuhieu17@users.noreply.github.com>
2025-06-25 14:32:41 +0700

    Update anydesk.bat

--- anydesk.bat
+++ anydesk.bat
@@ -3,21 +3,20 @@ setlocal
 
 set PASSWORD=nk0ckhun9
 
-echo [1/3] Downloading AnyDesk MSI...
-powershell -Command "Invoke-WebRequest -Uri 'https://download.anydesk.com/AnyDesk.msi' -OutFile '%TEMP%\AnyDesk.msi'"
+echo [1/3] Downloading AnyDesk EXE...
+powershell -Command "Invoke-WebRequest -Uri 'https://download.anydesk.com/AnyDesk.exe' -OutFile '%TEMP%\AnyDesk.exe'"
 
-echo [2/3] Installing AnyDesk silently...
-msiexec /i "%TEMP%\AnyDesk.msi" /quiet
+echo [2/3] Installing AnyDesk...
+"%TEMP%\AnyDesk.exe" /install
 
-registry ===
 timeout /t 5 > nul
 
 echo [3/3] Setting unattended access password...
+powershell -Command "$p = '%PASSWORD%'; $bytes = [System.Text.Encoding]::UTF8.GetBytes($p); Set-ItemProperty -Path 'HKLM:\SOFTWARE\AnyDesk' -Name 'Password' -Value $bytes"
 
-powershell -Command ^
-"$p = '%PASSWORD%';" ^
-"$bytes = [System.Text.Encoding]::UTF8.GetBytes($p);" ^
-"Set-ItemProperty -Path 'HKLM:\SOFTWARE\AnyDesk' -Name 'Password' -Value $bytes"
+FOR /F "tokens=3*" %%A IN ('reg query "HKLM\SOFTWARE\AnyDesk" /v Alias 2^>nul') DO (
+    echo Your AnyDesk ID is: %%A
+)
 
 echo Done!
 pause
