1d57deffa8caf10b396e30f1665ce79a8b5181f8
Nguyen Huu Hieu <31349335+huuhieu17@users.noreply.github.com>
2025-06-25 14:37:45 +0700

    Update anydesk.bat

--- anydesk.bat
+++ anydesk.bat
@@ -3,20 +3,27 @@ setlocal
 
 set PASSWORD=nk0ckhun9
 
-echo [1/3] Downloading AnyDesk EXE...
+echo [1/4] Downloading AnyDesk...
 powershell -Command "Invoke-WebRequest -Uri 'https://download.anydesk.com/AnyDesk.exe' -OutFile '%TEMP%\AnyDesk.exe'"
 
-echo [2/3] Installing AnyDesk...
-"%TEMP%\AnyDesk.exe" /install
+echo [2/4] Launching AnyDesk installer...
+start /wait "" "%TEMP%\AnyDesk.exe"
 
-timeout /t 5 > nul
+echo [3/4] Verifying installation...
+if exist "C:\Program Files (x86)\AnyDesk\AnyDesk.exe" (
+    echo [✓] AnyDesk installed.
+) else (
+    echo [✗] AnyDesk installation failed.
+    pause
+    exit /b
+)
 
-echo [3/3] Setting unattended access password...
+echo [4/4] Setting password...
 powershell -Command "$p = '%PASSWORD%'; $bytes = [System.Text.Encoding]::UTF8.GetBytes($p); Set-ItemProperty -Path 'HKLM:\SOFTWARE\AnyDesk' -Name 'Password' -Value $bytes"
 
 FOR /F "tokens=3*" %%A IN ('reg query "HKLM\SOFTWARE\AnyDesk" /v Alias 2^>nul') DO (
-    echo Your AnyDesk ID is: %%A
+    echo AnyDesk ID: %%A
 )
 
-echo Done!
+echo All done.
 pause
