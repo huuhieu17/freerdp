a1c0727c0f3321ef63bbb3490861621bcd63c0ae
Steve <31349335+huuhieu17@users.noreply.github.com>
2025-11-11 23:35:28 +0700

    Simplify PowerShell command to single line

--- get_public_ip.bat
+++ get_public_ip.bat
@@ -5,10 +5,8 @@ REM File to save JSON
 set "JSONFILE=%TEMP%\public_ip.json"
 if exist "%JSONFILE%" del "%JSONFILE%" >nul 2>&1
 
-REM Run PowerShell to get public IP and save as JSON
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ^
-  "$ip = Invoke-RestMethod 'https://api.ipify.org'; ^ 
-   @{ip=$ip} | ConvertTo-Json | Out-File '%JSONFILE%' -Encoding UTF8"
+REM --- Run PowerShell as a single line ---
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command " $ip = Invoke-RestMethod 'https://api.ipify.org'; @{ip=$ip} | ConvertTo-Json | Out-File '%JSONFILE%' -Encoding UTF8 "
 
 REM Display the JSON
 if exist "%JSONFILE%" (
